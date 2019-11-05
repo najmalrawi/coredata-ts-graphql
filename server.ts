54f760b1063da9025ccdc31dbd8bdc717381ed64
najmalrawi <najmalrawi@gmail.com>
2019-11-05 07:15:20 +0500

    Created GraphQL Server

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-// GraphQL Server
+import { GraphQLServer } from "graphql-yoga"
+import { default as typeDefs } from "./server/types/types"
+import { default as resolvers } from "./server/resolvers/resolvers"
+import { default as options } from "./server/graphql-options"
+
+const server = new GraphQLServer({ typeDefs, resolvers, })
+
+server.start(options, () => {
+
+	console.log(`Server is running on localhost:${options.port}`)
+
+}).catch(error => {
+
+	console.error("Connection error", error)
+})
